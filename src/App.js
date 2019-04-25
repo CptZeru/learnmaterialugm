02ec09c92d61cb9813246214d3f6c975b6bcbc39
CptZeru <zerulegend@gmail.com>
2019-04-25 09:02:59 +0700

    belajar material ugm

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Product from './Product';
+import YourAppBar from './YourAppBar';
+
+class App extends Component {
+  render(){
+    return(
+      <YourAppBar />
+    )
+  }
+  
+}
+
+export default App;
